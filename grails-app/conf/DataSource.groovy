f0060914862745f6a8454af9272db2e98efdc4f3
Rubén Cahuich Cahuich <rbnseven@Rubens-MacBook-Pro.local>
2015-09-19 00:14:52 -0500

    File config for enviroments.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -17,30 +17,9 @@ hibernate {
 // environment specific settings
 environments {
     development {
-        dataSource {
-            pooled = true
-            driverClassName = "com.mysql.jdbc.Driver"
-            username = "root"
-            password = ""
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:mysql://localhost:3306/campestre_dev?autoReconnect=true&characterEncoding=utf-8"
-            properties {
-                maxActive = 50
-                maxIdle = 25
-                minIdle = 1
-                initialSize = 1
-                minEvictableIdleTimeMillis = 60000
-                timeBetweenEvictionRunsMillis = 60000
-                numTestsPerEvictionRun = 3
-                maxWait = 10000
-
-                testOnBorrow = true
-                testWhileIdle = true
-                testOnReturn = false
-
-                validationQuery = "SELECT 1"
-            }
-        }
+        grails.config.locations = [
+                "file:${userHome}/grails-configs/campestre-dev.groovy"
+        ]
     }
     test {
         dataSource {
@@ -49,29 +28,8 @@ environments {
         }
     }
     production {
-        dataSource {
-            pooled = true
-            driverClassName = "com.mysql.jdbc.Driver"
-            username = "root"
-            password = ""
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:mysql://localhost:3306/campestre?autoReconnect=true&characterEncoding=utf-8"
-            properties {
-                maxActive = 50
-                maxIdle = 25
-                minIdle = 1
-                initialSize = 1
-                minEvictableIdleTimeMillis = 60000
-                timeBetweenEvictionRunsMillis = 60000
-                numTestsPerEvictionRun = 3
-                maxWait = 10000
-
-                testOnBorrow = true
-                testWhileIdle = true
-                testOnReturn = false
-
-                validationQuery = "SELECT 1"
-            }
-        }
+        grails.config.locations = [
+                "file:${userHome}/grails-configs/campestre-prod.groovy"
+        ]
     }
 }
